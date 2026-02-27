a9ae3b77158b465879fda3c4dbeb50894d09bb85
MuriloZague <zaguemurilo7@gmail.com>
2026-02-27 07:56:29 -0300

    feat: adding a searchbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,54 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet, View, Text } from 'react-native';
+import { Image } from "expo-image";
+import { Platform, StyleSheet, View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { HelloWave } from "@/components/hello-wave";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Link } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const [text, setText] = useState("");
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-    <View>
-      <View style={styles.headerApp}>
-        <Image
-          style={{ width: 155, height: 30 }}
-          source={require('../../assets/images/logoReceitalhada.png')}
-        />
-        <Image
-        style={{ width:35, height: 35}}
-        source={require('../../assets/images/profile-icon.svg')}
-        />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <View style={styles.headerApp}>
+          <Image
+            style={{ width: 155, height: 30 }}
+            source={require("../../assets/images/logoReceitalhada.png")}
+          />
+          <Image
+            style={{ width: 35, height: 35 }}
+            source={require("../../assets/images/profile-icon.svg")}
+          />
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>
+            Encontre A <Text style={{ color: "#E96B35" }}>Melhor Receita</Text>{" "}
+            Para A Sua Fome
+          </Text>
+          <View style={styles.inputWrapper}>
+            <Ionicons
+              name="search"
+              size={20}
+              color="#888"
+              style={styles.searchIcon}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={setText}
+              value={text}
+              placeholder="Procure por uma receita"
+              placeholderTextColor='#999999'
+              keyboardType="default"
+            />
+          </View>
+        </View>
       </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Encontre A <Text style={{color: '#E96B35'}}>Melhor Receita</Text> Para A Sua Fome</Text>
-      </View>
-    </View>
     </SafeAreaView>
   );
 }
@@ -35,20 +58,21 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingBottom: 16,
     marginTop: 16,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderBottomWidth: 8,
-    borderBottomColor: '#E96B35'
+    borderBottomColor: "#E96B35",
   },
   titleContainer: {
-    paddingHorizontal: 50,
-    paddingTop: 20,
+    paddingHorizontal: 30,
+    paddingTop: 25,
   },
   title: {
     fontSize: 28,
-    textAlign: 'center',
+    textAlign: "center",
     fontWeight: 500,
+    paddingHorizontal: 20,
   },
   stepContainer: {
     gap: 8,
@@ -59,6 +83,24 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+  inputWrapper: {
+    marginTop: 25,
+    position: "relative",
+    justifyContent: "center",
+  },
+  searchIcon: {
+    position: "absolute",
+    left: 14,
+    zIndex: 1,
+  },
+  input: {
+    height: 45,
+    backgroundColor: "#dfdfdf",
+    paddingLeft: 42,
+    paddingRight: 16,
+    borderRadius: 100,
+    fontSize: 15,
   },
 });
