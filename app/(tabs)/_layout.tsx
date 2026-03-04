24e799f360309f19b341e96acb897675c8498fff
MuriloZague <zaguemurilo7@gmail.com>
2026-03-04 15:07:59 -0300

    refactor: renaming components and retreving errors on imports

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,7 +34,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="login"
         options={{
           title: "Profile",
           tabBarIcon: ({ color }) => (
