92b8524de6e6d7430e95d6e61bd05a0499dfb74c
MuriloZague <zaguemurilo7@gmail.com>
2026-04-24 19:22:07 -0300

    feat: making all attributes required

--- src/models/Recipe.ts
+++ src/models/Recipe.ts
@@ -1,8 +1,8 @@
 export interface Recipe {
     id?: string;
-    recipeName: string;
-    category: string;
-    prepTime: number;
-    ingredients: string;
-    instructions: string;
+    recipeName?: string;
+    category?: string;
+    prepTime?: number;
+    ingredients?: string;
+    instructions?: string;
 }
